38f457d56d5422bed03fd8791a7fbef8ba0479e3
Yuns <yuns.xie@qq.com>
2024-06-15 10:01:14 +0800

    ♻️ chore: update deps

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,4 +1,10 @@
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+
 import janna from '@jannajs/lint/eslint'
+
+const __filename = fileURLToPath(import.meta.url) // get the resolved path to the file
+const __dirname = path.dirname(__filename) // get the name of the directory
 
 export default janna({
   next: {
