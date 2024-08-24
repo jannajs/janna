2b282287b7a584656f6598806cbf458b23dd217c
Yuns <yuns.xie@qq.com>
2024-08-24 19:25:00 +0800

    ♻️ chore: husky init

--- packages/lint/src/core.ts
+++ packages/lint/src/core.ts
@@ -78,7 +78,7 @@ export async function preparePackageJson() {
 
   const eslintCli = 'eslint --flag unstable_ts_config'
 
-  packageJson.setScript('prepare', 'husky install')
+  packageJson.setScript('prepare', 'husky init')
     .setScript('lint', `${eslintCli} .`)
     .setScript('lint:fix', `${eslintCli} .`)
 
